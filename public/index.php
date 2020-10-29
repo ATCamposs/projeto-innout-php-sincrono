3ecdbcdca32ff86bbe151e976e659b8420d6efe9
ATCamposs <andre.tatibano@gmail.com>
2020-10-29 16:10:13 -0300

    Adição de css e mudanças visualização

--- public/index.php
+++ public/index.php
@@ -1,14 +1,4 @@
 <?php
 
 require_once __DIR__ . '/../src/Config/Config.php';
-
-use Src\Config\Database;
-use Src\Model\User;
-
-$user = new User(['name' => 'André', 'email' => 'andre.email@email.com']);
-echo '<br>';
-var_dump(User::getAll(['id' => 1], 'name, email'));
-echo '<br>';
-echo '<br>';
-echo '<br>';
-var_dump(User::getAll([], 'name'));
+require_once __DIR__ . '/../src/View/login.php';
