17d122aadcd96b567348037a26070003bc6d1a8b
ATCamposs <andre.tatibano@gmail.com>
2020-11-10 18:09:07 -0300

    Adicionada funções workedInterval

--- src/Model/WorkingHours.php
+++ src/Model/WorkingHours.php
@@ -2,6 +2,7 @@
 
 namespace Src\Model;
 
+use DateInterval;
 use DateTime;
 use Src\Exceptions\AppException;
 use Src\Model\Model;
@@ -83,5 +84,39 @@ class WorkingHours extends Model
         if (empty($id)) {
             $this->insert();
         }
+    }
+
+    private function getWorkedInterval(): DateInterval
+    {
+        [$t1, $t2, $t3, $t4] = $this->getTimes();
+        $part1 = new DateInterval('PT0S');
+        $part2 = new DateInterval('PT0S');
+
+        if ($t1) {
+            $part1 = $t1->diff(new DateTime());
+        }
+        if ($t2 && $t1 instanceof DateTime) {
+            $part1 = $t1->diff($t2);
+        }
+        if ($t3) {
+            $part2 = $t3->diff(new DateTime());
+        }
+        if ($t4 && $t3 instanceof DateTime) {
+            $part2 = $t3->diff($t4);
+        }
+
+        return sumIntervals($part1, $part2);
+    }
+
+    /** @return array<int, Datetime|false|null> */
+    private function getTimes()
+    {
+        $times = [];
+
+        $this->time1 ? array_push($times, getDateFromString($this->time1)) : array_push($times, null);
+        $this->time2 ? array_push($times, getDateFromString($this->time2)) : array_push($times, null);
+        $this->time3 ? array_push($times, getDateFromString($this->time3)) : array_push($times, null);
+        $this->time4 ? array_push($times, getDateFromString($this->time4)) : array_push($times, null);
+        return $times;
     }
 }
